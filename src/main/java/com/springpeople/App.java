ae8b90ba85a315cda838a1ed7f357a55be887f85
arunkumar gunasekaran <arunkumar_gunasekaran@intui.com>
2015-04-18 15:41:44 +0530

    update

--- src/main/java/com/springpeople/App.java
+++ src/main/java/com/springpeople/App.java
@@ -6,8 +6,15 @@ package com.springpeople;
  */
 public class App 
 {
+	/**Main method*/
     public static void main( String[] args )
     {
         System.out.println( "Hello World! with github repository change" );
     }
+	/**sample method*/
+	public void sample()
+	{
+	    System.out.println("testing by arun");
+		}
+		
 }
